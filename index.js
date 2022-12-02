703e1be67ab39f3c302f5777dbcb94ffd7514458
shariandabre <dabresharian@gmail.com>
2022-12-02 08:46:28 +0530

    commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const navbar = document.querySelector("#navbar");
 const navbtn = document.querySelector(".menu");
-
+function change(){
+  navbar.setAttribute("visible", "false");
+  navbtn.setAttribute("aria-expand", "flase");
+  document.getElementById('navbar').style.display='none'
+}
 navbtn.addEventListener("click", () => {
   const open = navbar.getAttribute("visible");
 
   if (open == "false") {
     navbar.setAttribute("visible", "true");
     navbtn.setAttribute("aria-expand", "true");
+    document.getElementById('navbar').style.display='flex'
   } else if (open == "true") {
     navbar.setAttribute("visible", "false");
     navbtn.setAttribute("aria-expand", "flase");
+    document.getElementById('navbar').style.display='none'
   }
 });
 
